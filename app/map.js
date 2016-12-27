fae44343faffc9a3c088294dc2227d628ff5b959
offroader <giorgishvelidze@gmail.com>
2016-12-27 12:31:23 +0400

    feature selection

--- app/map.js
+++ app/map.js
@@ -13,18 +13,23 @@ function createMap (config) {
     })
     
     var myTreesLayer = new ol.layer.Vector({
-      source: new ol.source.Vector(),
-      style: new ol.style.Style({
-          image: new ol.style.Icon({
-            anchor: [0.5, 48],
-            anchorXUnits: 'fraction',
-            anchorYUnits: 'pixels',
-            src: 'static/icons/white-tree.png'
-          })
-        })
+      source: new ol.source.Vector()
     })
         
     map.addLayer(myTreesLayer)
+    
+    var selectInteraction = new ol.interaction.Select()
+    map.addInteraction(selectInteraction)
+    
+    selectInteraction.getFeatures().on('add', function (e) {
+        var feature = e.element
+        feature.setStyle(getSelectedStyle())
+    })
+    
+    selectInteraction.getFeatures().on('remove', function (e) {
+        var feature = e.element
+        feature.setStyle(getNormalStyle())
+    })
     
     setTimeout(function () {
         config.getRandomPoints(function (points) {
@@ -43,7 +48,31 @@ function createMap (config) {
             geometry: new ol.geom.Point(ol.proj.fromLonLat([data.lon, data.lat]))
         })
         
+        f.setStyle(getNormalStyle())
+        
         return f
+    }
+    
+    function getNormalStyle () {
+        return new ol.style.Style({
+          image: new ol.style.Icon({
+            anchor: [0.5, 48],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            src: 'static/icons/white-tree.png'
+          })
+        })
+    }
+    
+    function getSelectedStyle () {
+        return new ol.style.Style({
+          image: new ol.style.Icon({
+            anchor: [0.5, 48],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            src: 'static/icons/white-tree-selected.png'
+          })
+        })
     }
     
     return map
